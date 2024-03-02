2bf1b7d09bf9bd71e056107f24e51b227b795dbf
shruti111191 <152438578+shruti111191@users.noreply.github.com>
2024-03-02 13:44:27 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,58 @@
+import java.util.*;
+ public class Main
+{
+	public static void main(String[] args) {
+	    int i = 1;
+	    
+	    while(i <= 9)
+	    {
+		System.out.println(i);
+		i = i + 1;
+	    }
+	}
+}
+
+
+
+
+
+
+import java.util.*;
+ public class Main
+{
+	public static void main(String[] args){
+	Scanner scn = new Scanner(System.in);
+	{
+	  int n = scn.nextInt();
+	  for(int i = 0; i <= n; i++)
+	    
+	    
+	    {
+		System.out.println(i);
+		
+	    }}
+	}
+}
+
+
+
+import java.util.*;
+ public class Main
+{
+	public static void main(String[] args){
+	Scanner scn = new Scanner(System.in);
+	int n = integer.parseInt(scn.nextline());
+	
+	
+	  String name = scn.nextLine();
+	    
+	    
+	    {
+		System.out.println( " Hello "  +name " Here is the counting " );
+		for(int i = 1 ; i <= n; i++)
+		{
+		System.out.println(i);
+		
+	    }}
+	}
+}
